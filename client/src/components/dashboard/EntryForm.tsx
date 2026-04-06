d7c1e79d8b1e1233c4012d9d4ee27e0ec7a82845
Himendra <fernandohimendra@gmail.com>
2026-04-07 00:23:32 +1200

    Created validation checks for when making an entry.

--- client/src/components/dashboard/EntryForm.tsx
+++ client/src/components/dashboard/EntryForm.tsx
@@ -18,6 +18,7 @@ const CATEGORIES: EntryCategory[] = [
     "Personal"
 ];
 
+//Form Data State
 type FormData = {
     title: string;
     value: string;
@@ -26,6 +27,13 @@ type FormData = {
     note: string;
 };
 
+type Error = {
+    title: string;
+    value: string;
+    date: string;  
+};
+
+//Sets the initial form with empty or preset values
 function getInitialFormData(editingEntry: Entry | null): FormData {
     if (editingEntry) {
         return {
@@ -46,6 +54,14 @@ function getInitialFormData(editingEntry: Entry | null): FormData {
     };
 }
 
+function getInitialErrors(): Error {
+    return {
+        title: "",
+        value: "",
+        date: "",
+    };
+}
+
 //Method renders a form to handle adding an entry to the dashboard
 export default function EntryForm({
     onSubmitEntry,
@@ -56,6 +72,37 @@ export default function EntryForm({
         getInitialFormData(editingEntry)
     );
 
+    const [errors, setErrors] = useState<Error>(() => getInitialErrors());
+
+    //Checks the submitting or editing entry for errors
+    function validateForm(): boolean{
+        const nextErrors: Error = {
+            title: "",
+            value: "",
+            date: "",
+        };
+
+        if(formData.title.trim() === "") {
+            nextErrors.title = "Title is required.";
+        };
+
+        if (!formData.value.trim()) {
+            nextErrors.value = "Value is required.";
+        } else if(Number.isNaN(Number(formData.value))) {
+            nextErrors.value = "Value must be a valid number.";
+        } else if(Number(formData.value) <= 0) {
+            nextErrors.value = "Value must be greater than 0.";
+        };
+
+        if (!formData.date) {
+            nextErrors.date = "Date is Required";
+        };
+
+        setErrors(nextErrors);
+
+        return !nextErrors.title && !nextErrors.value && !nextErrors.date;
+    }
+
     function handleChange(
         event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
     ) {
@@ -65,18 +112,23 @@ export default function EntryForm({
             ...currentFormData,
             [name]: value,
         }));
+
+        if (name === "title" || name === "value" || name === "date") {
+            setErrors((currentErrors) => ({
+                ...currentErrors,
+                [name]: "",
+            }));
+        }
     }
 
     //This runs when a form is submitted
     function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault(); //Prevents the form submission from reloading the page
 
+        const isValid = validateForm();
+    
         //Checks if required fields are missing
-        if (
-            !formData.title.trim() ||
-            !formData.value.trim() ||
-            !formData.date
-        ) {
+        if (!isValid) {
             return;
         }
 
@@ -93,6 +145,8 @@ export default function EntryForm({
         //Passes the entry to the parent through the function prop
         onSubmitEntry(submittedEntry);
 
+        setErrors(getInitialErrors);
+        
         //Resetting all the form fields to their default values
         if(!editingEntry){
             setFormData(getInitialFormData(null));
@@ -112,8 +166,15 @@ export default function EntryForm({
                     value={formData.title}
                     onChange={handleChange}
                     placeholder="e.g. Study Hours"
-                    className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition focus:border-slate-500"
+                    className={`w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition ${
+                        errors.title
+                          ? "border-red-500 focus:border-red-500"
+                          : "border-slate-300 focus:border-slate-500"
+                    }`}
                 />
+                {errors.title && (
+                    <p className="mt-2 text-sm text-red-600">{errors.title}</p>
+                )}
             </div>
 
             {/*Value input field*/}
@@ -128,8 +189,15 @@ export default function EntryForm({
                     value={formData.value}
                     onChange={handleChange}
                     placeholder="e.g. 2.5"
-                    className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition focus:border-slate-500"
+                    className={`w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition ${
+                        errors.value
+                          ? "border-red-500 focus:border-red-500"
+                          : "border-slate-300 focus:border-slate-500"
+                    }`}
                 />
+                {errors.value && (
+                    <p className="mt-2 text-sm text-red-600">{errors.value}</p>
+                )}
             </div>
 
             {/*Category select field*/}
@@ -138,8 +206,9 @@ export default function EntryForm({
                     Category
                 </label>
                 <select
+                    name="category"
                     value={formData.category}
-                    onChange={(event) => setFormData({...formData, category: event.target.value as EntryCategory})}
+                    onChange={handleChange}
                     className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition focus:border-slate-500"
                     >
                         {CATEGORIES.map((Option) => (
@@ -160,8 +229,15 @@ export default function EntryForm({
                     name="date"
                     value={formData.date}
                     onChange={handleChange}
-                    className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition focus:border-slate-500"
+                    className={`w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition ${
+                        errors.date
+                          ? "border-red-500 focus:border-red-500"
+                          : "border-slate-300 focus:border-slate-500"
+                    }`}
                 />
+                {errors.date && (
+                    <p className="mt-2 text-sm text-red-600">{errors.date}</p>
+                )}
             </div>
 
             {/*Note input field*/}
@@ -170,8 +246,9 @@ export default function EntryForm({
                     Note
                 </label>
                 <textarea
+                    name="note"
                     value={formData.note}
-                    onChange={(event) => setFormData({...formData, note: event.target.value})}
+                    onChange={handleChange}
                     placeholder="Optional note"
                     rows={4}
                     className="w-full rounded-xl border border-slate-300 px-4 py-3 text-sm outline-none transition focus:border-slate-500"
